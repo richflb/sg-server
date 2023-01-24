7c579b58c85673d46bb547bb5d42e7bf3cd18c13
Richardson Florencio <richardson.florencio5@gmail.com>
2023-01-24 16:17:08 -0300

    some deletations...

--- src/controllers/bkp_cadastrarController.js
+++
@@ -1,7 +0,0 @@
-exports.cadastrar = (request, response) => {
-  response.render("cadastrar");
-};
-
-exports.cadastroFinalizado = (request, response) => {
-  response.render("cadastroFinalizado");
-};
