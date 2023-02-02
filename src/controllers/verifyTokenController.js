283865b1e331fc7155eb365095b46378c40146b6
Richardson Florencio <richardson.florencio5@gmail.com>
2023-02-02 16:26:31 -0300

    Adding Service of Token Verify...

---
+++ src/controllers/verifyTokenController.js
@@ -0,0 +1,35 @@
+const { PrismaClient } = require('@prisma/client');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const prisma = new PrismaClient();
+
+exports.verifyToken = async (req, res) => {
+    const accessToken = req.token;
+    if (!accessToken) return res.status(400).json({ 'message': 'Username and password are required.' });
+    
+    const auth = await prisma.users.findUnique({
+            where: {email: req.user},
+            include: {
+                Authentication: true,
+                Profile: true
+            }
+        });
+
+        let authToken = auth?.Authentication[0].refreshToken
+        if(accessToken === authToken){
+            const { id, userName, Profile } = auth;
+
+            return res.status(200).json({
+                uid: id,
+                username: userName,
+                authToken,
+                Profile
+            });
+        }
+    
+        res.sendStatus(401);
+}
+
+//module.exports = { verifyToken };
--- src/middleware/verifyJWT.js
+++ src/middleware/verifyJWT.js
@@ -2,20 +2,21 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const verifyJWT = (req, res, next) => {
-    console.log(req)
+
     const authHeader = req.headers['authorization'];
-    console.log(">>>> authHeader", authHeader, "\n>>>>>> User", req.user)
-    if (!authHeader) return res.sendStatus(401);
+    if (!authHeader) {
+console.log("401 REQQQQQ >>>>>")
+		return res.sendStatus(401);}
      // Bearer token
     const token = authHeader.split(' ')[1];
     jwt.verify(
         token,
         process.env.ACCESS_TOKEN_SECRET,
         (err, decoded) => {
-            console.log(err)
-            console.log(">>>>>> decoded:", decoded)
             if (err) return res.sendStatus(403); //invalid token
             req.user = decoded.email;
+		req.token = token;
+console.log(">>>> verify middleware >>>>>> User", req.user)
             next();
         }
     );
