f931c2344cd1018651322dee85dfe9d386d482df
Richardson Florencio <richardson.florencio5@gmail.com>
2023-02-02 16:30:33 -0300

    Adding Routes of Service of Token Verify...

--- src/routes/privateRoutes.js
+++ src/routes/privateRoutes.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const { verify } = require("jsonwebtoken");
 const route = express.Router();
 
 const homeController = require("../controllers/homeController");
 const logoutController = require("../controllers/logoutController");
+const verifyTokenController = require("../controllers/verifyTokenController");
+
 route.get("/home", homeController.home);
 route.get("/logout", logoutController.handleLogout);
+route.post("/verify", verifyTokenController.verifyToken)
 
 module.exports = route;
