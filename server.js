b974f8a106a48aa2ef4f75234a68751a44b91ba6
Richardson Florencio <richardson.florencio5@gmail.com>
2023-03-09 15:41:56 -0300

    temporaly server...

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const app = express();
 //app.use(credentials);
 
 //app.use(cors(corsOptions));
-app.use(cors());
+//app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
